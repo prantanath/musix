04b8e41efdbcd4ebd828d143a4919d9063ef17f0
Restyled.io <commits@restyled.io>
2024-08-22 06:43:07 +0000

    Restyled by clang-format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,ts}",                                                                      ],
-  theme: {
-    extend: {
-      fontFamily: {
-        "nunito": ['Nunito', 'sans-serif']
-      }
-    },
+  content :
+          [
+            "./src/**/*.{html,ts}",
+          ],
+  theme : {
+    extend : {fontFamily : {"nunito" : [ 'Nunito', 'sans-serif' ]}},
   },
-  plugins: [],
+  plugins : [],
 }
-
