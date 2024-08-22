c91cbb0dd4fa5c9cc5d3d5c46003297a8df9cbc6
Restyled.io <commits@restyled.io>
2024-08-22 06:43:09 +0000

    Restyled by prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content :
-          [
-            "./src/**/*.{html,ts}",
-          ],
-  theme : {
-    extend : {fontFamily : {"nunito" : [ 'Nunito', 'sans-serif' ]}},
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: { fontFamily: { nunito: ["Nunito", "sans-serif"] } },
   },
-  plugins : [],
-}
+  plugins: [],
+};
